d96cf6c8f4b322c97038b572ab7f873d52403330
NOOR OMARI <nooromari847@gmail.com>
2021-05-31 20:08:32 +0300

    scraper done

--- web_scraper/web_scraper.py
+++ web_scraper/web_scraper.py
@@ -3,6 +3,27 @@ from bs4 import BeautifulSoup
 
 
 URL = 'https://en.wikipedia.org/wiki/History_of_Mexico'
-page = requests.get(URL)
 
-print(page.content)
+def get_citations_needed_count(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    result = soup.find_all('sup', class_='noprint Inline-Template Template-Fact')
+    result_list = list(result)
+    length=len(result_list)
+    return length
+
+
+
+def get_citations_needed_report(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    result = soup.find_all('sup', class_='noprint Inline-Template Template-Fact')
+    report_result = [a.parent.text for a in result]
+    uniqe_res = set(report_result)
+    list_res = list(uniqe_res)
+    return list_res
+
+
+if __name__ == '__main__':
+    print(get_citations_needed_count(URL))
+    print(get_citations_needed_report(URL))
